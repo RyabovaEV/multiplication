ed4b9648efae2854962fbf014469093193c1acf4
RyabovaEV <elena.ryabova77m@gmail.com>
2025-04-26 11:45:52 +0300

    numbers

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 class MultiplicationApp {
-  constructor () {
+  constructor() {
     this.choiceNum = [];
-    this.dataChoiceNumContainer = document.querySelector("[data-choice-num-container]");
+    this.dataChoiceNumContainer = document.querySelector(
+      "[data-choice-num-container]"
+    );
     this.dataChoiceNumBtn = document.querySelector("[data-choice-num-btn]");
-    this.dataMultiplicationQuestion = document.querySelector("[data-multiplication-question]");
-    this.dataMultiplicationAnswer = document.querySelector("[data-multiplication-answer]");
-    this.dataMultiplicationFinish = document.querySelector("[data-multiplication-finish]");
+    this.dataMultiplicationQuestion = document.querySelector(
+      "[data-multiplication-question]"
+    );
+    this.dataMultiplicationAnswer = document.querySelector(
+      "[data-multiplication-answer]"
+    );
+    this.dataMultiplicationFinish = document.querySelector(
+      "[data-multiplication-finish]"
+    );
     this.dataResult = document.querySelector("[data-result]");
     this.bindEvents();
   }
@@ -14,8 +22,26 @@ class MultiplicationApp {
     this.dataChoiceNumContainer.addEventListener("click", (e) => {
       const el = e.target;
 
-      //if el.hasAttribute()
-    })
+      if (el.classList.contains("choice-num__item")) {
+        const wasPressed = el.getAttribute("aria-pressed") === "true";
+        const nowPressed = !wasPressed;
+
+        el.setAttribute("aria-pressed", nowPressed);
+        this.getChoiceNumArr(nowPressed, +el.textContent);
+
+        this.dataChoiceNumBtn.disabled = this.choiceNum.length === 0;
+      }
+    });
+  }
+
+  getChoiceNumArr(isPressed, choiceNum) {
+    if (isPressed) {
+      this.choiceNum.push(choiceNum);
+    } else {
+      if (this.choiceNum) {
+        this.choiceNum = this.choiceNum.filter((num) => num !== choiceNum);
+      }
+    }
   }
 }
 
